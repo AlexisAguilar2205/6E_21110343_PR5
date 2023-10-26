4f2c4f845caea01a178ad7d29b627019cfd0f520
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-10-26 00:21:43 -0600

    Árbol de Máximo y Mínimo coste Kruska

---
+++ _6E_21110343_PR5.py
@@ -0,0 +1,60 @@
+class Grafo:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.aristas = []
+
+    def agregar_arista(self, u, v, w):
+        self.aristas.append((u, v, w))
+
+    def encontrar(self, padre, i):
+        if padre[i] == i:
+            return i
+        return self.encontrar(padre, padre[i])
+
+    def unir(self, padre, rango, x, y):
+        raiz_x = self.encontrar(padre, x)
+        raiz_y = self.encontrar(padre, y)
+        if rango[raiz_x] < rango[raiz_y]:
+            padre[raiz_x] = raiz_y
+        elif rango[raiz_x] > rango[raiz_y]:
+            padre[raiz_y] = raiz_x
+        else:
+            padre[raiz_y] = raiz_x
+            rango[raiz_x] += 1
+
+    def kruskal(self, maximo=False):
+        resultado = []
+        i, e = 0, 0
+        self.aristas = sorted(self.aristas, key=lambda item: item[2], reverse=maximo)
+        padre = []
+        rango = []
+
+        for nodo in range(self.V):
+            padre.append(nodo)
+            rango.append(0)
+
+        while e < self.V - 1:
+            u, v, w = self.aristas[i]
+            i = i + 1
+            x = self.encontrar(padre, u)
+            y = self.encontrar(padre, v)
+            if x != y:
+                e = e + 1
+                resultado.append((u, v, w))
+                self.unir(padre, rango, x, y)
+
+        for u, v, w in resultado:
+            print(f"Arista {u} - {v}, Peso {w}")
+
+# Ejemplo de uso
+grafo = Grafo(4)
+grafo.agregar_arista(0, 1, 10)
+grafo.agregar_arista(0, 2, 6)
+grafo.agregar_arista(0, 3, 5)
+grafo.agregar_arista(1, 3, 15)
+grafo.agregar_arista(2, 3, 4)
+
+print("Árbol de Mínima Expansión (Kruskal):")
+grafo.kruskal()
+print("\nÁrbol de Máxima Expansión (Kruskal):")
+grafo.kruskal(maximo=True)
